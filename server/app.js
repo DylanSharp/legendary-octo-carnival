49da4afcb50110a2a8df29395b20e28062819561
Dylan Sharp <dylan.mark.sharp@gmail.com>
2022-07-11 13:33:35 +0200

    Add initial packages.

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", async (req, res) => {
+    res.end('Hello');
+});
+
 app.listen(3000, () => {
     console.log("Listening...");
 });
