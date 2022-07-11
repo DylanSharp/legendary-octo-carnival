1842af2afec91fd79a29d2d8d1f2644e8914d9e8
Dylan Sharp <dylan.mark.sharp@gmail.com>
2022-07-11 16:19:46 +0200

    Add basic required crud routes.
    Routes created for adding comments and upvotes and one for fetching all comments.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,51 @@
 const express = require("express");
-
-const app = express();
+const Upvote = require('./models/Upvote')
+const Comment = require('./models/Comment')
+const User = require('./models/User')
 
 require('./connection');
 
-app.get("/", async (req, res) => {
-    res.end('Hello');
+const app = express();
+app.use(express.json());
+
+app.get("/comments", async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    const allComments = await Comment.findAll()
+
+    res.end(JSON.stringify(allComments));
+});
+
+app.post("/upvote/:comment_id", async (req, res) => {
+    const commentId = req.params.comment_id;
+
+    // Ensure the comment_id is valid.
+    const comment = await Comment.findOne({
+        where: {
+            id: commentId
+        }
+    });
+    if (!comment) {
+        res.status(400).end('Invalid comment ID');
+    } else {
+        await Upvote.create({
+            comment_id: commentId
+        })
+        res.end(commentId);
+    }
+
+});
+
+app.post("/comment", async (req, res) => {
+    // Get a random user to assign the comment to.
+    const allUsers = await User.findAll();
+    const randomUserIndex = Math.floor(Math.random() * allUsers.length);
+    const randomUser = allUsers[randomUserIndex];
+
+    await Comment.create({
+        user_id: randomUser.id,
+        content: req.body['content'],
+    })
+    res.end(randomUserIndex);
 });
 
 app.listen(3000, () => {
