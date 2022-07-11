c597a08c061bca5bd915dcab1274bde435a664e2
Dylan Sharp <dylan.mark.sharp@gmail.com>
2022-07-11 13:57:45 +0200

    Fix db settings.

--- server/connection.js
+++ server/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 const sequelize = new Sequelize(
     'ghost_discussions ',
     'admin',
-    'GgelbHxxSFzylEde1SoR',
+    '.39Wu9AhsUu9hK2wiqB-2X6b',
     {
         host: 'localhost',
         dialect: 'mysql'
