df6a664acc4cac8131a687e69302c5b8b2994143
Dylan Sharp <dylan.mark.sharp@gmail.com>
2022-07-12 14:51:38 +0200

    Move helpers out of module. Add sort to comment list.

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -2,9 +2,22 @@ import '../styles/discussion.css';
 import Comment from "./Comment";
 
 const CommentList = (props) => {
+
+    const sortCommentData = (commentData) => {
+        return commentData.sort((a, b) => {
+            if (a.createdAt > b.createdAt) {
+                return -1;
+            }
+            if (a.createdAt < b.createdAt) {
+                return 1;
+            }
+            return 0;
+        })
+    }
+
     return (
         <section className="discussion__comment-list">
-            {props.comments ? props.comments.map(comment => {
+            {props.comments ? sortCommentData(props.comments).map(comment => {
                 return (
                     <Comment
                         key={comment.id}
