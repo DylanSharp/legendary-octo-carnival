e036f81ee89772353dbd498a1ff5bf1f581ab66c
Dylan Sharp <dylan.mark.sharp@gmail.com>
2022-07-12 06:01:57 +0200

    Refactor variable name to explicity imply it's an element.

--- client/js/index.js
+++ client/js/index.js
@@ -1,60 +1,13 @@
 /**
- * Takes a date as a string and returns a nice human-readable string like '2 days ago'
- * @param dateString A date string representing the time the comment was created.
- * @returns {string}
- */
-function timeSince(dateString) {
-    const date = new Date(dateString)
-    let secondsSince = Math.floor((new Date() - date) / 1000);
-
-    const sanitize = (intervalName, interval) => {
-        intervalName = interval > 1 ? intervalName + 's' : intervalName
-        return interval + ' ' + intervalName + ' ago'
-    }
-
-    let interval = secondsSince / 31536000;
-    if (interval > 1) {
-        return sanitize('year', Math.floor(interval));
-    }
-    interval = secondsSince / 2592000;
-    if (interval > 1) {
-        return sanitize('month', Math.floor(interval));
-    }
-    interval = secondsSince / 86400;
-    if (interval > 1) {
-        return sanitize('day', Math.floor(interval));
-    }
-
-    let now = new Date();
-    let then = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0)
-    const secondsSinceYesterday = Math.floor((now.getTime() - then.getTime()) / 1000);
-
-    interval = secondsSince / secondsSinceYesterday;
-    if (interval > 1) {
-        return "Yesterday";
-    }
-
-    interval = secondsSince / 3600;
-    if (interval > 1) {
-        return sanitize('hour', Math.floor(interval));
-    }
-    interval = secondsSince / 60;
-    if (interval > 1) {
-        return sanitize('minute', Math.floor(interval));
-    }
-    return sanitize('second', Math.floor(interval));
-}
-
-/**
  * Creates and returns a comment element.
  *
  * @param commentData An object representing a comment.
  * @returns {HTMLDivElement}
  */
 const createCommentElement = (commentData) => {
-    const comment = document.createElement('div');
-    comment.classList.add("comment");
-    comment.innerHTML = `
+    const commentElement = document.createElement('div');
+    commentElement.classList.add("comment");
+    commentElement.innerHTML = `
             <img src="https://avatars.dicebear.com/v2/avataaars/${commentData.userId}.svg"
                  alt="Current User Avatar Image"
                  class="comment__avatar">
@@ -77,19 +30,7 @@ const createCommentElement = (commentData) => {
                 </div>
             </div>`;
 
-    return comment;
-}
-
-const sortCommentData = (commentData) => {
-    return commentData.sort((a, b) => {
-        if (a.createdAt > b.createdAt) {
-            return -1;
-        }
-        if (a.createdAt < b.createdAt) {
-            return 1;
-        }
-        return 0;
-    })
+    return commentElement;
 }
 
 const renderComments = (commentData) => {
